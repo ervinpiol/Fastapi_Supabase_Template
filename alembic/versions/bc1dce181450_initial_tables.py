0acfe30e4827216e704e2d40591881d38f173ed7
Ervin Piol <rvnpiol14@gmail.com>
2026-01-16 14:53:05 +0800

    feat: alembic

---
+++ alembic/versions/bc1dce181450_initial_tables.py
@@ -0,0 +1,33 @@
+"""initial tables
+
+Revision ID: bc1dce181450
+Revises: 
+Create Date: 2026-01-16 14:50:44.556076
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'bc1dce181450'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        'users',
+        sa.Column('id', sa.Integer(), primary_key=True, autoincrement=True),
+        sa.Column('email', sa.String(255), nullable=False, unique=True, index=True),
+        sa.Column('is_active', sa.Boolean(), nullable=False, server_default='true'),
+        sa.Column('is_superuser', sa.Boolean(), nullable=False, server_default='false'),
+        sa.Column('full_name', sa.String(255), nullable=True),
+        sa.Column('hashed_password', sa.String(), nullable=False),
+    )
+
+def downgrade():
+    op.drop_table('users')
